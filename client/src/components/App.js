6f2a5a1e1aa12a5ec55ef8377793f84b98a66979
amitzed <amitzmn@gmail.com>
2020-07-29 19:28:22 -0400

    Imported new files into App.js and set up routes which are all working - workouts/new, workouts/new, workouts/edit, workouts/delete, workouts/show and /

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import WorkoutList from './workouts/WorkoutList';
+import WorkoutCreate from './workouts/WorkoutCreate';
+import WorkoutEdit from './workouts/WorkoutEdit';
+import WorkoutDelete from './workouts/WorkoutDelete';
+import WorkoutShow from './workouts/WorkoutShow';
+
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Route path="/" exact component={WorkoutList} />
+        <Route path="/workouts/new" exact component={WorkoutCreate} />
+        <Route path="/workouts/edit" exact component={WorkoutEdit} />
+        <Route path="/workouts/delete" exact component={WorkoutDelete} />
+        <Route path="/workouts/show" exact component={WorkoutShow} />
+      </BrowserRouter>
     </div>
   )
 };
