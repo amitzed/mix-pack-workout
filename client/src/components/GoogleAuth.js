c023defeb9f86fe51e46d3c5c67c3a1e53c7f3f9
amitzed <amitzmn@gmail.com>
2020-08-06 18:18:49 -0400

    Client: Edited GoogleAuth.js with logic to let user know whether they're signed in or not

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -4,19 +4,36 @@ const KEY = process.env.REACT_APP_GOOGLE_API_CLIENT_ID
 const SCOPE = process.env.REACT_APP_GOOGLE_API_SCOPE
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: KEY,
         scope: SCOPE
-      });
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({
+          isSignedIn: this.auth.isSignedIn.get()
+        })
+      })
     });
+  }
+
+  renderAuthBtn() {
+    if(this.state.isSignedIn === null) {
+      return <div>I can't tell if you're signed in</div>
+    } else if(this.state.isSignedIn) {
+      return <div>You're Signed In</div>
+    } else {
+      return <div>You're Not Signed In</div>
+    }
   }
 
   render() {
     return (
       <div>
-        Google Authentication
+        {this.renderAuthBtn()}
       </div>
     );
   }
