20361124ce83d2db0031de868c2a5eafea65ab1d
amitzed <amitzmn@gmail.com>
2020-08-05 21:07:29 -0400

    Google Auth now functional and can sign in and out via the console -- not set up on screen yet

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
+const KEY = process.env.REACT_APP_GOOGLE_API_CLIENT_ID
+const SCOPE = process.env.REACT_APP_GOOGLE_API_SCOPE
+
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: KEY,
+        scope: SCOPE
+      });
+    });
+  }
+
   render() {
     return (
       <div>
