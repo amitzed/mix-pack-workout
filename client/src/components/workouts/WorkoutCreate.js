2940ccf4c701af992532c005bbc01b258b218389
amitzed <amitzmn@gmail.com>
2020-08-16 17:22:32 -0400

    Form input data accurately showing that redux-form storing data inside reducer (using Redux Dev Tools to test)

--- client/src/components/workouts/WorkoutCreate.js
+++ client/src/components/workouts/WorkoutCreate.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class WorkoutCreate extends React.Component {
-  render() {
+  renderInput(formProps) {
     return (
       <div>
-        WorkoutCreate
+        <input
+          onChange={formProps.input.onChange}
+          value={formProps.input.value}
+        />
       </div>
+    )
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        <Field name="day" component={this.renderInput} />
+        <Field name="exercise1Title" component={this.renderInput} />
+        <Field name="exercise1Sets" component={this.renderInput} />
+        <Field name="exercise1Reps" component={this.renderInput} />
+        <Field name="exercise2Title" component={this.renderInput} />
+        <Field name="exercise2Sets" component={this.renderInput} />
+        <Field name="exercise2Reps" component={this.renderInput} />
+        <Field name="exercise3Title" component={this.renderInput} />
+        <Field name="exercise3Sets" component={this.renderInput} />
+        <Field name="exercise3Reps" component={this.renderInput} />
+        <Field name="cardioType" component={this.renderInput} />
+        <Field name="cardioTime" component={this.renderInput} />
+      </form>
     );
   }
 }
 
-export default WorkoutCreate;
+export default reduxForm({
+  form: 'workoutCreate'
+}) (WorkoutCreate);
