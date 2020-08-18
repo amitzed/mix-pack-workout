b72eae99fdcc68becad987df49095b995409aee0
amitzed <amitzmn@gmail.com>
2020-08-18 18:56:47 -0400

    Styled form on /workouts/new slug

--- client/src/components/workouts/WorkoutCreate.js
+++ client/src/components/workouts/WorkoutCreate.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class WorkoutCreate extends React.Component {
-  renderInput({ input }) {
+  renderInput({ input, label }) {
     return (
       <div>
-        <input {...input} />
+        <input {...input} placeholder={label} />
+
       </div>
     )
   }
@@ -13,20 +14,125 @@ class WorkoutCreate extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <form>
-        <Field name="day" component={this.renderInput} />
-        <Field name="exercise1Title" component={this.renderInput} />
-        <Field name="exercise1Sets" component={this.renderInput} />
-        <Field name="exercise1Reps" component={this.renderInput} />
-        <Field name="exercise2Title" component={this.renderInput} />
-        <Field name="exercise2Sets" component={this.renderInput} />
-        <Field name="exercise2Reps" component={this.renderInput} />
-        <Field name="exercise3Title" component={this.renderInput} />
-        <Field name="exercise3Sets" component={this.renderInput} />
-        <Field name="exercise3Reps" component={this.renderInput} />
-        <Field name="cardioType" component={this.renderInput} />
-        <Field name="cardioTime" component={this.renderInput} />
-      </form>
+      <div className="ui inverted segment">
+        <h2 class="ui center aligned icon header">
+          <i class="circular green flask icon"></i>
+          Enter Workout Details
+        </h2>
+        <form className="ui inverted form large">
+          <div className="fields ui one column center aligned grid">
+            <div className="five wide field">
+              <Field
+                name="day"
+                component={this.renderInput}
+                label="Day"
+              />
+            </div>
+          </div>
+
+          <div className="fields ui one column center aligned grid">
+            <div className="four wide field">
+              <Field
+                name="exercise1Title"
+                component={this.renderInput}
+                label="Exercise 1"
+              />
+            </div>
+          </div>
+
+            <div className="two fields ui column center aligned grid">
+              <div className="four wide field">
+                <Field
+                  name="exercise1Sets"
+                  component={this.renderInput}
+                  label="Sets"
+                />
+              </div>
+              <div className="four wide field">
+                <Field
+                  name="exercise1Reps"
+                  component={this.renderInput}
+                  label="Reps"
+                />
+              </div>
+            </div>
+
+          <div className="fields ui one column center aligned grid">
+            <div className="four wide field">
+              <Field
+                name="exercise2Title"
+                component={this.renderInput}
+                label="Exercise 2"
+              />
+            </div>
+          </div>
+
+            <div className="two fields ui column center aligned grid">
+              <div className="four wide field">
+                <Field
+                  name="exercise2Sets"
+                  component={this.renderInput}
+                  label="Sets"
+                />
+              </div>
+              <div className="four wide field">
+                <Field
+                  name="exercise2Reps"
+                  component={this.renderInput}
+                  label="Reps"
+                />
+              </div>
+            </div>
+
+          <div className="fields ui one column center aligned grid">
+            <div className="four wide field">
+              <Field
+                name="exercise3Title"
+                component={this.renderInput}
+                label="Exercise 3"
+              />
+            </div>
+          </div>
+
+            <div className="two fields ui column center aligned grid">
+              <div className="four wide field">
+                <Field
+                  name="exercise3Sets"
+                  component={this.renderInput}
+                  label="Sets"
+                />
+              </div>
+              <div className="four wide field">
+                <Field
+                  name="exercise3Reps"
+                  component={this.renderInput}
+                  label="Reps"
+                />
+              </div>
+            </div>
+
+          <div className="fields ui one column center aligned grid">
+            <div className="four wide field">
+              <Field
+                name="cardioType"
+                component={this.renderInput}
+                label="Cardio Type"
+              />
+            </div>
+          </div>
+
+          <div className="fields ui one column center aligned grid">
+            <div className="three wide field">
+              <Field
+                name="cardioTime"
+                component={this.renderInput}
+                label="Cardio Time"
+              />
+            </div>
+          </div>
+
+        </form>
+      </div>
     );
   }
 }
