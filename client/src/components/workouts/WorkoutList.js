4b1321d80de7c2c954c6d909bef065e98703edd0
amitzed <amitzmn@gmail.com>
2020-08-24 18:03:56 -0400

    Data now successfully displaying on Index/WorkoutList browser screen

--- client/src/components/workouts/WorkoutList.js
+++ client/src/components/workouts/WorkoutList.js
@@ -8,10 +8,28 @@ class WorkoutList extends React.Component {
     this.props.fetchWorkouts();
   }
 
+  renderList() {
+    return this.props.workouts.map(workout => {
+      return (
+        <div key={workout.id}>
+          <div>
+            {workout.day}
+            <div>
+              {workout.exercise1Title}
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        WorkoutList
+        <h2>WorkoutList</h2>
+        <div>
+          {this.renderList()}
+        </div>
       </div>
     );
   }
