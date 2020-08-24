c5981c1215761fb11ce98f94089ef36045890b2e
amitzed <amitzmn@gmail.com>
2020-08-24 17:16:06 -0400

    Created action creators for different CRUD operations

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import workouts from '../apis/workouts';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_WORKOUT,
+  FETCH_WORKOUTS,
+  FETCH_WORKOUT,
+  DELETE_WORKOUT,
+  EDIT_WORKOUT
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -14,6 +22,42 @@ export const signOut = () => {
   }
 };
 
+// CREATE Workout
 export const createWorkout = (formValues) => async (dispatch) => {
-  workouts.post('/workouts', formValues)
+  const response = await workouts.post('/workouts', formValues);
+
+  dispatch({
+    type: CREATE_WORKOUT,
+    payload: response.data
+  })
+}
+
+// Fetch all workouts
+export const fetchWorkouts = () => async (dispatch) => {
+  const response = await workouts.get('/workouts');
+
+  dispatch({
+    type: FETCH_WORKOUTS,
+    payload: response.data
+  })
+}
+
+// Fetch just ONE workout
+export const fetchWorkout = (id) => async (dispatch) => {
+  const response = await workouts.get(`/workouts/${id}`);
+
+  dispatch({
+    type: FETCH_WORKOUT,
+    payload: response.data
+  })
+}
+
+// EDIT workout
+export const editWorkout = (id, formValues) => async (dispatch) => {
+  const response = await workouts.put(`/workouts/${id}`, formValues);
+
+  dispatch({
+    type: EDIT_WORKOUT,
+    payload: response.data
+  })
 }
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_WORKOUT = 'CREATE_WORKOUT';
+export const FETCH_WORKOUTS = 'FETCH_WORKOUTS';
+export const FETCH_WORKOUT = 'FETCH_WORKOUT';
+export const DELETE_WORKOUT = 'DELETE_WORKOUT';
+export const EDIT_WORKOUT = 'EDIT_WORKOUT';
