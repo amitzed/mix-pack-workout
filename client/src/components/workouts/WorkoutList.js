8c902ecd2313fb546718d1bc0708a43adccc2b9f
amitzed <amitzmn@gmail.com>
2020-08-25 20:17:20 -0400

    Initial styling applied for WorkoutList.js screen

--- client/src/components/workouts/WorkoutList.js
+++ client/src/components/workouts/WorkoutList.js
@@ -11,27 +11,110 @@ class WorkoutList extends React.Component {
   renderList() {
     return this.props.workouts.map(workout => {
       return (
-        <div className="" key={workout.id}>
-          <div>
-            {workout.day}
-            <div>
-              Exercise: {workout.exercise1Title}
+        <div className="ui two column centered" key={workout.id}>
+          <div className="ui segments">
+
+            <div className="ui inverted brown center aligned segment" style={{margin: '1em'}}>
+              <p>{workout.day}</p>
             </div>
-            <div>
-              {
-                !workout.exercise1Sets
-                ?
-                <div>
-                  {workout.exercise1Sets}
-                  {workout.exercise1Reps}
+
+            <div className="ui segments">
+              <div className="ui inverted center aligned segment">
+
+                <p>
+                  {
+                    !workout.exercise1Title
+                    ?
+                    null
+                    :
+                    <span>Exercise 1: {workout.exercise1Title}</span>
+                  }
+                </p>
+                <div className="ui segments">
+                  {
+                    !workout.exercise1Sets
+                    ?
+                    null
+                    :
+                    <div className="ui horizontal segments">
+                      <div className="ui secondary inverted segment">
+                        <p>Sets: {workout.exercise1Sets}</p>
+                      </div>
+                      <div className="ui tertiary inverted segment">
+                        <p>Reps: {workout.exercise1Reps}</p>
+                      </div>
+                    </div>
+                  }
                 </div>
-                :
-                <div>
-                  Sets: {workout.exercise1Sets}
-                  Reps: {workout.exercise1Reps}
+                <p>
+                  {
+                    !workout.exercise2Title
+                    ?
+                    null
+                    :
+                    <span>Exercise 2: {workout.exercise2Title}</span>
+                  }
+                </p>
+                <div className="ui segments">
+                  {
+                    !workout.exercise2Sets
+                    ?
+                    null
+                    :
+                    <div className="ui horizontal segments">
+                      <div className="ui secondary inverted segment">
+                        <p>Sets: {workout.exercise2Sets}</p>
+                      </div>
+                      <div className="ui tertiary inverted segment">
+                        <p>Reps: {workout.exercise2Reps}</p>
+                      </div>
+                    </div>
+                  }
                 </div>
-              }
+                <p>
+                  {
+                    !workout.exercise3Title
+                    ?
+                    null
+                    :
+                    <span>Exercise 3: {workout.exercise3Title}</span>
+                  }
+                </p>
+                <div className="ui segments">
+                  {
+                    !workout.exercise3Sets
+                    ?
+                    null
+                    :
+                    <div className="ui horizontal segments">
+                      <div className="ui secondary inverted segment">
+                        <p>Sets: {workout.exercise3Sets}</p>
+                      </div>
+                      <div className="ui tertiary inverted segment">
+                        <p>Reps: {workout.exercise3Reps}</p>
+                      </div>
+                    </div>
+                  }
+                </div>
+
+                {
+                  !workout.cardioTime || !workout.cardioType
+                  ?
+                  null
+                  :
+                  <div className="ui segments">
+                    <p>Cardio: {workout.cardioType}</p>
+                    <div className="ui horizontal segments">
+                      <div className="ui secondary inverted segment">
+                        <p>Time: {workout.cardioTime}</p>
+                      </div>
+                    </div>
+                  </div>
+                }
+
+              </div>
             </div>
+
           </div>
         </div>
       )
@@ -40,9 +123,9 @@ class WorkoutList extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="">
         <h2>WorkoutList</h2>
-        <div className="ui centered grid">
+        <div className="ui stackable four column grid">
           {this.renderList()}
         </div>
       </div>
