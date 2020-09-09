8a6b8e1196ea12a7e802612e4083373a3575faf1
amitzed <amitzmn@gmail.com>
2020-09-09 18:28:44 -0400

    On delete Modal, in addition to clicking cancel button, if user clicks outside of modal text box, user is programmatically redirected to root page

--- client/src/components/DimmerModal.js
+++ client/src/components/DimmerModal.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import history from '../history';
+
 const DimmerModal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
+    <div className="ui dimmer modals visible active" onClick={() => history.push('/')}>
+      <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()}>
 
         <div className="header">Delete This Workout</div>
         <div className="content">
