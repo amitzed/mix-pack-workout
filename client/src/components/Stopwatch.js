5d7455c94fc6d8e53773c0c6796de7e9a3eee45b
amitzed <amitzmn@gmail.com>
2020-09-11 11:42:42 -0400

    Altered stopwatch button to display a modal when clicked

--- client/src/components/Stopwatch.js
+++ client/src/components/Stopwatch.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import history from '../history';
+
 class Stopwatch extends React.Component {
 
   state = {
@@ -41,25 +43,32 @@ class Stopwatch extends React.Component {
 
       return (
 
-        <div className="stopwatch-app">
+        <div className="ui dimmer modals visible active stopwatch-app-container" onClick={() => history.push('/')}>
+          <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()} style={{background: '#4b0082'}}>
+            <br/>
+            <h5>Click Outside This Box To Return To Home Screen</h5>
+            <div className="stopwatch-app">
+              <div className="stopwatch-header"><i className="stopwatch icon"></i></div>
+              <div className="stopwatch-display">
+                {minutes} : {seconds} : {centiseconds}
+              </div>
+              {this.state.timerOn === false && this.state.timerTime === 0 && (
+                <button onClick={this.startTimer}><i className="play icon"></i></button>
+              )}
+              {this.state.timerOn === true && (
+                <button onClick={this.stopTimer}><i className="pause icon"></i></button>
+              )}
+              {this.state.timerOn === false && this.state.timerTime > 0 && (
+                <button onClick={this.startTimer}>Resume</button>
+              )}
+              {this.state.timerOn === false && this.state.timerTime > 0 && (
+                <button onClick={this.resetTimer}><i className="undo icon"></i></button>
+              )}
+            </div>
 
-          <div className="stopwatch-header"><i className="stopwatch icon"></i></div>
-          <div className="stopwatch-display">
-            {minutes} : {seconds} : {centiseconds}
           </div>
-          {this.state.timerOn === false && this.state.timerTime === 0 && (
-            <button onClick={this.startTimer}><i class="play icon"></i></button>
-          )}
-          {this.state.timerOn === true && (
-            <button onClick={this.stopTimer}><i class="pause icon"></i></button>
-          )}
-          {this.state.timerOn === false && this.state.timerTime > 0 && (
-            <button onClick={this.startTimer}>Resume</button>
-          )}
-          {this.state.timerOn === false && this.state.timerTime > 0 && (
-            <button onClick={this.resetTimer}><i class="undo icon"></i></button>
-          )}
         </div>
+
       );
     }
 }
