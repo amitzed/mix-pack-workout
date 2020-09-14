ad24259a9769fd9f35ef961c0c68807cc1f12aaa
amitzed <amitzmn@gmail.com>
2020-09-14 18:53:04 -0400

    Changed WorkoutDelete to a class-based components and hooked it up to fetchWorki=out Action Creator

--- client/src/components/workouts/WorkoutDelete.js
+++ client/src/components/workouts/WorkoutDelete.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import DimmerModal from '../DimmerModal';
 import history from '../../history';
+import { fetchWorkout } from '../../actions';
 
-const WorkoutDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <div className="ui inverted blue button">Cancel</div>
-      <div className="ui negative button">Delete</div>
-    </React.Fragment>
-  );
+class WorkoutDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchWorkout(this.props.match.params.id)
+  }
 
-  return (
-    <div>
-      <DimmerModal
-        title="Delete This Workout"
-        content="This action is permanent, are you sure?"
-        actions={actions}
-        modalDismiss={() => history.push('/')}
-      />
-    </div>
-  );
+  renderActions() {
+    return (
+      <React.Fragment>
+        <div className="ui inverted blue button">Cancel</div>
+        <div className="ui negative button">Delete</div>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <DimmerModal
+          title="Delete This Workout"
+          content="This action is permanent, are you sure?"
+          actions={this.renderActions()}
+          modalDismiss={() => history.push('/')}
+        />
+      </div>
+    );
+  }
 }
 
-export default WorkoutDelete;
+export default connect(
+  null,
+  { fetchWorkout }
+)(WorkoutDelete);
