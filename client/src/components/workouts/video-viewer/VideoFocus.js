60832a25fce6fad5b7eb5734593c92601757f1c0
amitzed <amitzmn@gmail.com>
2020-09-24 20:27:31 -0400

    On /workouts/videos on list click videos are showing in iframe

--- client/src/components/workouts/video-viewer/VideoFocus.js
+++ client/src/components/workouts/video-viewer/VideoFocus.js
@@ -14,11 +14,10 @@ const VideoFocus = ({ video }) => {
   return (
     <div className="jumbotron workout-video-focus-wrapper" >
       <div className="embed-responsive embed-responsive-16by9">
-        {/* <iframe className="embed-responsive-item" src={videoSource} title="Video Player" /> */}
+        <iframe className="embed-responsive-item" src={videoSource} title="Video Player" /> 
       </div>
       <hr className="my-3" />
       <div className="lead">
-        VideoFocus.js - Video-Viewer
         <h4>{video.snippet.title}</h4>
         <h6>{video.snippet.description}</h6>
       </div>
